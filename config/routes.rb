6d086bc04c864c12ca7c1857127dff2ab9a166b4
rscheurman <riley.scheurman@gmail.com>
2018-07-10 14:42:26 -0600

    - basic tweet integration - basic pages controller integration

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :tweets
+  get 'pages/home'
+  get 'pages/profile'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
